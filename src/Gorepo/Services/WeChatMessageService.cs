8dfa4c926ef9c773ae703bf59df66b2637d82f4e
hwz <40764272+he55@users.noreply.github.com>
2020-06-07 17:28:02 +0800

    Update WeChatMessageService.cs

--- src/Gorepo/Services/WeChatMessageService.cs
+++ src/Gorepo/Services/WeChatMessageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
@@ -15,7 +16,7 @@ namespace Gorepo.Services
             _httpClient = httpClientFactory.CreateClient("wed");
         }
 
-        public object GetMessageInfo(string xmlMessage)
+        public Dictionary<string, string> GetMessageInfo(string xmlMessage)
         {
             XmlDocument xmlDocument = new XmlDocument();
             xmlDocument.LoadXml(xmlMessage);
@@ -26,35 +27,35 @@ namespace Gorepo.Services
                 return xmlNode == null ? "" : xmlNode.InnerText;
             }
 
-            var messageInfo = new
+            Dictionary<string, string> messageInfo = new Dictionary<string, string>
             {
-                fromusername = GetNodeValueString("/msg/fromusername"),
-                appmsg_title = GetNodeValueString("/msg/appmsg/title"),
-                appmsg_des = GetNodeValueString("/msg/appmsg/des"),
-                appmsg_template_id = GetNodeValueString("/msg/appmsg/template_id"),
+                ["fromusername"] = GetNodeValueString("/msg/fromusername"),
+                ["appmsg_title"] = GetNodeValueString("/msg/appmsg/title"),
+                ["appmsg_des"] = GetNodeValueString("/msg/appmsg/des"),
+                ["appmsg_template_id"] = GetNodeValueString("/msg/appmsg/template_id"),
 
-                appinfo_appname = GetNodeValueString("/msg/appinfo/appname"),
-                appinfo_version = GetNodeValueString("/msg/appinfo/version"),
-                publisher_username = GetNodeValueString("/msg/appmsg/mmreader/publisher/username"),
-                publisher_nickname = GetNodeValueString("/msg/appmsg/mmreader/publisher/nickname"),
-                header_title = GetNodeValueString("/msg/appmsg/mmreader/template_header/title"),
-                header_pub_time = GetNodeValueString("/msg/appmsg/mmreader/template_header/pub_time"), // 10 位时间戳
+                ["appinfo_appname"] = GetNodeValueString("/msg/appinfo/appname"),
+                ["appinfo_version"] = GetNodeValueString("/msg/appinfo/version"),
+                ["publisher_username"] = GetNodeValueString("/msg/appmsg/mmreader/publisher/username"),
+                ["publisher_nickname"] = GetNodeValueString("/msg/appmsg/mmreader/publisher/nickname"),
+                ["header_title"] = GetNodeValueString("/msg/appmsg/mmreader/template_header/title"),
+                ["header_pub_time"] = GetNodeValueString("/msg/appmsg/mmreader/template_header/pub_time"), // 10 位时间戳
 
                 // 收款金额
-                detail_content_key_0 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/topline/key/word"),
-                detail_content_value_0 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/topline/value/word"),
+                ["detail_content_key_0"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/topline/key/word"),
+                ["detail_content_value_0"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/topline/value/word"),
 
                 // 收款方备注
-                detail_content_key_1 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[1]/key/word"),
-                detail_content_value_1 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[1]/value/word"),
+                ["detail_content_key_1"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[1]/key/word"),
+                ["detail_content_value_1"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[1]/value/word"),
 
                 // 汇总
-                detail_content_key_2 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[2]/key/word"),
-                detail_content_value_2 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[2]/value/word"),
+                ["detail_content_key_2"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[2]/key/word"),
+                ["detail_content_value_2"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[2]/value/word"),
 
                 // 备注
-                detail_content_key_3 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[3]/key/word"),
-                detail_content_value_3 = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[3]/value/word")
+                ["detail_content_key_3"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[3]/key/word"),
+                ["detail_content_value_3"] = GetNodeValueString("/msg/appmsg/mmreader/template_detail/line_content/lines/line[3]/value/word")
             };
 
             return messageInfo;
