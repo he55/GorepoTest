28486fffc89097ebbdcebfe10707e33982ae24f3
hwz <40764272+he55@users.noreply.github.com>
2020-06-07 16:21:34 +0800

    add

--- src/Gorepo/Context/HWZContext.cs
+++ src/Gorepo/Context/HWZContext.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gorepo.Context
 {
     public class HWZContext : DbContext
     {
+        public HWZContext([NotNull] DbContextOptions<HWZContext> options)
+            : base(options)
+        {
+        }
 
+        public DbSet<HWZMessage> Messages { get; set; } = null!;
+    }
+
+    public class HWZMessage
+    {
+        public int Id { get; set; }
+        public int CreateTime { get; set; }
+        public string ServerId { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public int PublishTime { get; set; }
+        public string OrderId { get; set; } = null!;
+        public decimal OrderAmount { get; set; }
     }
 }
--- src/Gorepo/Worker.cs
+++ src/Gorepo/Worker.cs
@@ -2,8 +2,10 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Gorepo.Context;
 using Gorepo.Models;
 using Gorepo.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -12,19 +14,29 @@ namespace Gorepo
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IServiceProvider _serviceProvider;
         private readonly WeChatMessageService _messageService;
-
         private int _timestamp;
 
-        public Worker(ILogger<Worker> logger, WeChatMessageService messageService)
+        public Worker(ILogger<Worker> logger,
+            IServiceProvider serviceProvider,
+            WeChatMessageService messageService)
         {
             _logger = logger;
+            _serviceProvider = serviceProvider;
             _messageService = messageService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _timestamp = 0;
+            {
+                HWZContext hwzContext = _serviceProvider.CreateScope()
+                    .ServiceProvider
+                    .GetRequiredService<HWZContext>();
+
+                _timestamp = hwzContext.Messages.Max(x => (int?)x.CreateTime) ?? 0;
+            }
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
@@ -32,8 +44,26 @@ namespace Gorepo
                 try
                 {
                     WeChatMessageItem[] messages = await _messageService.GetWeChatMessageItemsAsync(_timestamp);
+
                     if (messages.Length > 0)
                     {
+                        HWZContext hwzContext = _serviceProvider.CreateScope()
+                            .ServiceProvider
+                            .GetRequiredService<HWZContext>();
+
+                        foreach (WeChatMessageItem message in messages)
+                        {
+                            hwzContext.Messages.Add(new HWZMessage
+                            {
+                                CreateTime = message.Timestamp,
+                                ServerId = message.MessageId,
+                                Content = message.Message,
+                                OrderId = "id"
+                            });
+                        }
+
+                        await hwzContext.SaveChangesAsync();
+
                         _timestamp = messages.Max(x => x.Timestamp);
                     }
                 }
